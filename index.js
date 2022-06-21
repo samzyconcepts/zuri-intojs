5d8f52c66cbc75088c1a1baca4eb2e869e8eac47
samzyconcepts <samzyolufemi@gmail.com>
2022-06-21 08:12:57 +0100

    #1 commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const name = document.querySelector('.name'),
+    height = document.querySelector('.height'),
+    country = document.querySelector('.country');
+
+name.innerHTML = 'Oderinwale Samson';
+height.innerHTML = 
